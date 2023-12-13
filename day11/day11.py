21a7c6eb18a488245584a96a1fded14cee3fe703
Michael DeMarco <michaelfromyeg@gmail.com>
2023-12-12 16:05:18 -0800

    feat: stubs for day 11 and 12

---
+++ day11/day11.py
@@ -0,0 +1,30 @@
+"""
+https://adventofcode.com/2023/day/11
+"""
+from utils.read import read
+
+DEBUG = True
+DAY = 11
+
+
+def solve1() -> int:
+    """
+    Shortest paths between all planets in a galaxy.
+
+    Empty rows, columns "cost" double.
+    """
+    lines = read(DAY, 1 if DEBUG else 0)
+
+    return 0
+
+
+def solve2() -> int:
+    """ """
+    lines = read(DAY, 1 if DEBUG else 0)
+
+    return 0
+
+
+if __name__ == "__main__":
+    print(solve1())
+    print(solve2())
---
+++ day12/day12.py
@@ -0,0 +1,28 @@
+"""
+https://adventofcode.com/2023/day/12
+"""
+from utils.read import read
+
+DEBUG = True
+DAY = 0
+
+
+def solve1() -> int:
+    """
+    Sum possible arrangements for damaged springs.
+    """
+    lines = read(DAY, 1 if DEBUG else 0)
+
+    return 0
+
+
+def solve2() -> int:
+    """ """
+    lines = read(DAY, 1 if DEBUG else 0)
+
+    return 0
+
+
+if __name__ == "__main__":
+    print(solve1())
+    print(solve2())
